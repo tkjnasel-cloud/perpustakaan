dd7d715b381110e3585ee48a4ac0a4411975de76
tkjnasel-cloud <tkjnasel@gmail.com>
2026-07-07 12:02:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const btntambah = document.getElementById("btntambah");
+const listbuku = document.getElementById("listbuku");
+const totalbuku = document.getElementById("totalbuku")
+
+let jumlahbuku = 0
+
+// tombol tambah buku
+btntambah.addEventListener("click", tambahbuku);
+
+function tambahbuku() {
+    const judul = document.getElementById("judul").value;
+    const penulis = document.getElementById("penulis").value;
+    const kategori = document.getElementById("kategori").value;
+    const tahun = document.getElementById("tahun").value;
+    const status = document.getElementById("status").value;
+
+    //validasi
+    if (
+        judul === "" ||
+        penulis === "" ||
+        kategori === "" ||
+        tahun === ""
+    ) {
+        alert("lengkapi semua data buku!");
+        return;
+    }
+
+    //membuat card buku
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+        <h3> 📖 ${judul}</h3>
+
+         <p><b>Penulis :</b> ${penulis}</p>
+
+        <p><b>Kategori :</b> ${kategori}</p>
+
+        <p><b>Tahun :</b> ${tahun}</p>
+
+        <p><b>Status :</b> ${status}</p>
+
+        <div class="aksi">
+
+            <button class="hapus">
+                🗑 Hapus
+            </button>
+
+        </div>
+    `;
+
+    //tombol hapus
+
+    card.querySelector(".hapus").addEventListener("click", function() {
+        card.remove();
+        jumlahbuku --;
+        totalbuku.innerText = jumlahbuku;
+    });
+
+    //tampilkan buku
+    listbuku.appendChild(card);
+    jumlahbuku++;
+    totalbuku.innerText = jumlahbuku;
+
+    //kosongkan form
+    document.getElementById("judul").value = "";
+    document.getElementById("penulis").value = "";
+    document.getElementById("kategori").value = "";
+    document.getElementById("tahun").value = "";
+    document.getElementById("status").selectedIndex = 0;
+}    
